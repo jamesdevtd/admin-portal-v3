2684618b8ee01ace0906a240b4182b8116b49ae2
James FL <jlacandula@gmail.com>
2022-04-05 21:12:01 +0800

    reset-password

---
+++ src/pages/forgot-password.tsx
@@ -0,0 +1,23 @@
+import Header from '@/components/layout/Header';
+import Layout from '@/components/layout/Layout';
+
+
+export default function Dashboard() {
+  return (
+    <Layout layoutName='' pageTitle='Forgot password'>
+      <div className="">
+        <div className='content'>
+          <div className='content-wrap'>
+            <div className='m-auto flex w-full flex-col gap-5'>
+              <section className='my-40 flex flex-col items-center'>
+                <h3 className='text-lg'>
+                  FORGOT PASSWORD PAGE....
+                </h3>
+              </section>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+}
