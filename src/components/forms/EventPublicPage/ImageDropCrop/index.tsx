7b8f35347d14626c562a22659221c76a80bd0514
James FL <jlacandula@gmail.com>
2022-05-04 20:56:11 +0800

    dasdasd

--- src/components/forms/EventPublicPage/ImageDropCrop/index.tsx
+++ src/components/forms/EventPublicPage/ImageDropCrop/index.tsx
@@ -3,12 +3,9 @@ import React, { useEffect, useState } from 'react'
 // React Dropzone
 import { useDropzone } from 'react-dropzone';
 
-import styles from '@/components/forms/styles/FormGroup.module.scss';
-
 import { useAppDispatch, useAppSelector } from '@/app/hooks';
 import { getEventMainImage, updateMainImage } from '@/features/eventCreation/eventPublicPageSlice';
 
-import ImageIcon from '~/icons/grey/image.svg';
 import ImagePlaceholder from '~/svg/image-placeholder.svg';
 
 type Props = {
@@ -80,35 +77,30 @@ export default function MainEventImage({ eventId }: Props) {
     </div>
 
   return (
-    <div className={styles.formGroup}>
-      <ImageIcon />
-      <div className='label'>
-        <span>Main Event Image* </span>
-      </div>
-      <p className='instructions'>This is the first image athletes will see at the top of your event page and listing card. <br />Use a high quality image: 2160x1080px (2:1 ratio).</p>
-      <div className="fields-group">
-        <section className="dropzone-box">
-
-          <div {...getRootProps({ className: 'dropzone' })}>
-            <input {...getInputProps()} />
-            {thumb}
+    <section className="dropzone-box">
+      <div {...getRootProps({ className: 'dropzone' })}>
+        <input {...getInputProps()} />
+        {imgObject.output ?
+          thumb :
+          <>
             <div className="error">{error}</div>
             <ImagePlaceholder />
             <div className="instructions">
               <h3>Drag and drop some files here, or click to select files</h3>
               <p>JPEG or PNG, no larger than 10MB.</p>
             </div>
-          </div>
+          </>
+        }
 
-          {(files.length > 0 || imgObject.output) &&
-            <div className="flex gap-5 mt-3">
-              <button className='btn grey' onClick={removeImages}>Remove Image</button>
-              <button className='btn' onClick={reCrop}>Crop Image</button>
-            </div>
-          }
-        </section>
       </div>
-    </div>
+
+      {(files.length > 0 || imgObject.output) &&
+        <div className="flex gap-5 mt-3">
+          <button className='btn grey' onClick={removeImages}>Remove Image</button>
+          <button className='btn' onClick={reCrop}>Crop Image</button>
+        </div>
+      }
+    </section>
   )
 }
 
--- src/components/forms/EventPublicPage/index.tsx
+++ src/components/forms/EventPublicPage/index.tsx
@@ -1,58 +1,32 @@
-import { yupResolver } from '@hookform/resolvers/yup';
+import dynamic from 'next/dynamic'
 import React, { forwardRef, useEffect, useImperativeHandle, useState } from 'react';
+import { EditorProps } from 'react-draft-wysiwyg';
 import { useForm } from 'react-hook-form';
-import * as yup from 'yup';
 
 import 'react-datepicker/dist/react-datepicker.css';
-import styles from './EventPublicPage.module.scss';
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+import groupStyles from '@/components/forms/styles/FormGroup.module.scss';
 
 import { useAppDispatch } from '@/app/hooks';
 import { setCurrentStep, setIsEditedById } from '@/features/eventCreation/eventCreationSlice';
 
-import MainEventImage from './ImageDropCrop/MainEventImage';
+import MainEventImage from './ImageDropCrop';
 
 import CloseIcon from '~/icons/close.svg';
 import ErrorIcon from '~/icons/error.svg';
-
-const schema = yup
-  .object({
-    additionalEvents: yup.array(),
-    contactDetails: yup.array(),
-    eventEndDate: yup.date().required().nullable(),
-    eventName: yup.string().required(),
-    eventStartDate: yup.date().required(),
-    eventStartTime: yup.date().required(),
-    eventYear: yup.number().positive().integer().required().nullable(),
-    facilityAddress: yup.array().required(),
-    facilityName: yup.string().required(),
-    facilityNotes: yup.string().nullable(),
-    registrationEndDate: yup.date().required(),
-    registrationStartDate: yup.date().required(),
-    seriesMonth: yup.number().positive().integer().required().nullable(),
-  })
-  .required();
-
-const formDefaultValues = {
-  additionalEvents: [{}],
-  contactDetails: [{}],
-  eventEndDate: null,
-  eventName: null,
-  eventStartDate: null,
-  eventStartTime: null,
-  eventYear: null,
-  facilityAddress: [{}],
-  facilityName: null,
-  facilityNotes: '',
-  registrationEndDate: null,
-  registrationStartDate: null,
-  seriesMonth: null,
-}
+import DescriptionIcon from '~/icons/grey/description.svg';
+import ImageIcon from '~/icons/grey/image.svg';
 
 
 type Props = {
   step: number,
   eventStatus: { id: number, status: string }
 }
+
+const Editor = dynamic<EditorProps>(
+  () => import('react-draft-wysiwyg').then((mod) => mod.Editor),
+  { ssr: false }
+)
 
 export const EventPublicPage = forwardRef(({ step, eventStatus, ...props }: Props, ref) => {
 
@@ -61,17 +35,22 @@ export const EventPublicPage = forwardRef(({ step, eventStatus, ...props }: Prop
   const [hasErrors, setHasErrors] = useState(false);
   const [hideErrorBox, setHideErrorBox] = useState(false);
 
+  const [editorVal, setEditorVal] = useState('');
+
+  const onHtmlChange = (e: any) => {
+    // setHtml(e.target.value);
+    console.log('onHtmlChange: ', e);
+
+  }
+
   const {
     handleSubmit,
     getValues,
     setValue,
     formState,
   } = useForm({
-    resolver: yupResolver(schema),
     mode: 'onSubmit',
-    defaultValues: formDefaultValues
   });
-
 
 
   const setIsFormEdited = () => {
@@ -97,21 +76,6 @@ export const EventPublicPage = forwardRef(({ step, eventStatus, ...props }: Prop
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [formState.errors, formState.isDirty]);
 
-  const clearSelectedDates = () => {
-    setValue('seriesMonth', null);
-    setValue('eventStartDate', null);
-    setValue('registrationStartDate', null);
-  }
-
-  const setAvaiableSeries = (year: number) => {
-    const d = new Date();
-    const startMonthNumber = (year === d.getFullYear() && d.getDate() >= 7) ? d.getMonth() + 1 : null;
-    clearSelectedDates();
-  }
-
-
-  // TODO: transfter googleApiKey to .env
-
 
   const onSubmit = (data: unknown) => {
     //TODO: POST request to API
@@ -136,7 +100,7 @@ export const EventPublicPage = forwardRef(({ step, eventStatus, ...props }: Prop
       className={`main-form ${hasErrors ? 'has-errors' : ''}`}
     >
       {(hasErrors && !hideErrorBox) &&
-        <div className={styles.errorBox}>
+        <div className={groupStyles.errorBox}>
           <div className="wrap">
             <ErrorIcon />
             <span>Please Complete All Required Fields </span>
@@ -147,8 +111,48 @@ export const EventPublicPage = forwardRef(({ step, eventStatus, ...props }: Prop
 
       <h3>Event Public Page</h3>
 
-      <MainEventImage eventId={eventStatus.id} />
+      <div className={groupStyles.formGroup}>
+        <ImageIcon />
+        <div className='label'>
+          <span>Main Event Image* </span>
+        </div>
+        <p className='instructions'>This is the first image athletes will see at the top of your event page and listing card. <br />Use a high quality image: 2160x1080px (2:1 ratio).</p>
+        <MainEventImage eventId={eventStatus.id} />
 
+      </div>
+
+      <div className={groupStyles.formGroup}>
+        <DescriptionIcon />
+        <div className='label'>
+          <span>Description</span>
+        </div>
+        <p className='instructions'>Add more details to your Event - these details will be shown to <br />Teams who are searching for Events to join, so make sure to include why they should register and what they can look forward to! </p>
+
+        <div className="draggable-fields mt-5">
+
+          <section>
+            {/* <DefaultEditor value={html} onChange={onHtmlChange} /> */}
+            <Editor
+              // editorState={editorVal}
+              toolbarClassName="toolbarClassName"
+              wrapperClassName="wrapperClassName"
+              editorClassName="editorClassName"
+              onEditorStateChange={onHtmlChange}
+            />
+          </section>
+
+          <section>
+            <MainEventImage eventId={eventStatus.id} />
+          </section>
+          <section>
+            <div className="box-input">
+              <input type="text" placeholder='Video URL (Vimeo &amp; YouTube links currently supported)' />
+            </div>
+          </section>
+
+        </div>
+
+      </div>
 
     </div >
   );
