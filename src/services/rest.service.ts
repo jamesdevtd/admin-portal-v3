7e524ce0d5ef08765b3ebd154538c95b31882105
James FL <jlacandula@gmail.com>
2022-05-05 21:56:34 +0800

    fix

--- src/services/rest.service.ts
+++ src/services/rest.service.ts
@@ -11,7 +11,11 @@ axios.defaults.baseURL = process.env.NEXT_PUBLIC_API_BASE_URL;
  * @param options {params: <for query params>, payload: <for data to be sent>'}
  */
 
-const request = (method: Method, url: string, options = { params: {}, payload: {} }) => {
+const request = (
+  method: Method,
+  url: string,
+  options = { params: {}, payload: {} }
+) => {
   const request = {
     url,
     method,
@@ -22,46 +26,49 @@ const request = (method: Method, url: string, options = { params: {}, payload: {
   return new Promise((resolve, reject) => {
     axios
       .request(request)
-      .then(res => resolve(res))
-      .catch(err => reject(err));
+      .then((res) => resolve(res))
+      .catch((err) => reject(err));
   });
 };
 
 export const POST = (path: string, payload: any) => {
   return request('POST', path, {
     payload,
-    params: {}
+    params: {},
   });
 };
 
 export const UPLOAD = (path: string, payload: any) => {
   return axios.post(path, payload, {
-    onUploadProgress: ProgressEvent => (ProgressEvent.loaded / ProgressEvent.total) * 100,
+    onUploadProgress: (ProgressEvent) =>
+      (ProgressEvent.loaded / ProgressEvent.total) * 100,
   });
 };
 
 export const GET = (path: string, params: any) => {
   return request('GET', path, {
     params,
-    payload: undefined
+    // TODO: add typed payload if possible
+    payload: {} as any,
   });
 };
 
 export const GETALL = (path: string, filters: any) => {
   return request('GET', path, {
     params: filters,
-    payload: undefined
+    payload: {} as any,
   });
 };
 
 export const PUT = (path: string, payload: any) => {
-  return request('PUT', path, { payload });
+  return request('PUT', path, { ...payload });
 };
 
 export const PATCH = (path: string, payload: any) => {
   return request('PATCH', path, {
     payload: payload,
-    params: undefined
+    // TODO: add typed payload if possible
+    params: {} as any,
   });
 };
 
@@ -69,9 +76,9 @@ export const DELETE = (path: string) => {
   return request('DELETE', path);
 };
 
-axios.interceptors.request.use(request => {
+axios.interceptors.request.use((request) => {
   const token = Cookie.get('s_TxgKyE');
-  let headers = {};
+  const headers = {} as any;
   if (token) {
     headers['x-auth-token'] = `${token}`;
   }
@@ -85,11 +92,11 @@ axios.interceptors.request.use(request => {
  * RESPONSE INTERCEPTOR
  */
 axios.interceptors.response.use(
-  response => {
+  (response) => {
     // Do something with response data
     return response.data;
   },
-  error => {
+  (error) => {
     if (error.response.status === 401) {
       Cookie.remove('s_TxgKyE');
       Router.push('/');
@@ -98,5 +105,5 @@ axios.interceptors.response.use(
     //   return error.response.data;
     // }
     return Promise.reject(error.response.data);
-  },
+  }
 );
